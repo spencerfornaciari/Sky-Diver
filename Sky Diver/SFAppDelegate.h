4355f048941255f7c40c14c1c77d83688f7f7445
Spencer Fornaciari <mail@spencerfornaciari.com>
2014-02-18 23:53:47 -0800

    Initial Commit

---
+++ Sky Diver/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  Sky Diver
+//
+//  Created by Spencer Fornaciari on 2/18/14.
+//  Copyright (c) 2014 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sky Diver/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sky Diver
+//
+//  Created by Spencer Fornaciari on 2/18/14.
+//  Copyright (c) 2014 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SFAppDelegate class]));
+    }
+}
